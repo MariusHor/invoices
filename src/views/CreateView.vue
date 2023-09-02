5cb93503cb6211ebb0d5eba3eccb0bded6e44166
Marius <marius.horghidan@yahoo.com>
2023-09-02 14:01:40 +0300

    feat(create-route): add new route /invoices/create and create corresponding view component

---
+++ src/views/CreateView.vue
@@ -0,0 +1,20 @@
+<script setup></script>
+
+<template>
+  <h1 class="section-header">Factura noua</h1>
+</template>
+
+<style scoped lang="sass">
+.section-header
+  margin-bottom: 4rem
+
+.section-content
+  display: flex
+  flex-direction: column
+  gap: 1.575rem
+  height: 100%
+.section-actions
+  display: flex
+  justify-content: space-between
+  align-items: center
+</style>
